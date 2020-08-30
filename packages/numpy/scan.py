22d16dbc752b8b7a6b5bd541a17fd94c15a4739a
bwoodsend <bwoodsend@gmail.com>
2020-08-30 10:23:59 +0100

    Name jsons to include uuids
    Prevents name collisions.

--- packages/numpy/scan.py
+++ packages/numpy/scan.py
@@ -20,7 +20,7 @@ results = Sniff(None, body).to_json()
 
 from pathlib import Path
 import os
-
-name = os.environ.get("MATRIX_NAME", "scan")
+import uuid
+name = os.environ.get("MATRIX_NAME", "scan") + "-" + str(uuid.uuid1())
 
 Path(__file__).with_name(name + ".json").write_text(results)
